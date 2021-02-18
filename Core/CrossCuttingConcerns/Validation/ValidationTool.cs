26429afa157525f4f530bdcd98c672292936502e
orkun konak <orkun.konak@hotmail.com>
2021-02-18 22:03:25 +0300

    Core Katmanı Eklendi

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate<T>(IValidator validator, T entity)
+        {
+            var context = new ValidationContext<T>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
